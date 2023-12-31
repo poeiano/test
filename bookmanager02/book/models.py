aebffaddec078001651e0a4ba67f8dca35a6f160
poiano <1650847298@qq.com>
2023-12-31 11:29:14 +0800

    增删改查

--- bookmanager02/book/models.py
+++ bookmanager02/book/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class BookInfo(models.Model):
+
+    name = models.CharField(max_length=10,unique=True)
+    pub_date = models.DateField(null=True)
+    readcount = models.IntegerField(default=0)
+    commentcount = models.IntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'bookinfo'
+        verbose_name = '书籍管理'
+
+    def __str__(self):
+        return self.name
+
+class PeopleInfo(models.Model):
+
+    gender_choice = (
+        (1,'male'),
+        (2,'famale')
+    )
+
+    name = models.CharField(max_length=10,unique=True)
+    gender = models.SmallIntegerField(choices=gender_choice,default=1)
+    description = models.CharField(max_length=100,null=True)
+    is_delete = models.BooleanField(default=False)
+    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)     #级联操作
+
+    class Meta:
+        db_table = 'peopleinfo'
+
+    def __str__(self):
+        return self.name
--- bookmanager02/book/views.py
+++ bookmanager02/book/views.py
@@ -4,3 +4,50 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("index")
+
+'''
+#增加数据
+#方法一
+from book.models import BookInfo
+book = BookInfo(
+        name='django',
+        pub_date='2001-1-1',
+        readcount=10
+    )
+#方法二
+BookInfo.objects.create(
+        name='测试开发',
+        pub_date='2002-1-1',
+        readcount=10
+)
+
+#修改数据
+#方法一
+book = BookInfo.objects.get(id=6)
+book.name = '运维开发'
+book.save()
+#方式二
+BookInfo.objects.filter(id=6).update(name='爬虫入门',commentcount=666)  #仅限fileter使用update
+
+#删除数据
+#方法一
+book = BookInfo.objects.get(id=1)
+book.delete()
+#方法二
+BookInfo.objects.get(id=6).delete()
+BookInfo.objects.filter(id=5).delete()
+'''
+#F对象实现二者比较
+from book.models import BookInfo
+from django.db.models import F
+BookInfo.objects.filter(readcount__gte=F('commmentcount'))
+
+#并且查询
+BookInfo.objects.filter(readcount__gt=20).filter(commentcount_gt=10)
+BookInfo.objects.filter(readcount__gt=20,id__lt=3)
+
+#或者查询
+from  django.db.models import Q
+#或者语法格式:  BookInfo.objects.filter(Q(属性名__运算符=值)|Q(属性名__运算符=值)|...)
+#并且语法格式:  BookInfo.objects.filter(Q(属性名__运算符=值)&Q(属性名__运算符=值)&...)
+#非语法格式:  BookInfo.objects.filter(~Q(属性名__运算符=值))

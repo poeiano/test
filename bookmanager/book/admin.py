50fa456f9a929de47146b2b5e3f1f8db72d2a9d9
poeiano <1650847298@qq.com>
2026-03-16 14:58:29 +0800

    clear all

--- bookmanager/book/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from book.models import BookInfo,PeopleInfo
-admin.site.register(BookInfo)
-admin.site.register(PeopleInfo)
--- bookmanager/book/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from book.views import index
-urlpatterns = [
-    #path(路由，视图函数名)
-    path('index/',index)
-]
